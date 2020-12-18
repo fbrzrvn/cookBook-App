f56778d762ccfc1f737902614ee31f044af2fa25
fabrizio <faber.rvn@gmail.com>
2020-12-18 17:57:57 +0100

    Add a carousel slider Render dynamically the recipe by category

--- assets/js/controller/data.js
+++ assets/js/controller/data.js
@@ -7,5 +7,28 @@ const saveRecipe = (recipe) => {
   recipesList.push(recipe);
 }
 
+const renderRecipe = () => {
+  let recipes = JSON.parse(localStorage.getItem('allRecipe'));
+  recipes.forEach( el => {
+    let div = document.createElement('div');
+    div.className = 'recipe';
+    div.innerHTML = `<img src="${el.image}" alt="food">`;
+    switch (el.category) {
+      case 'healthy and veggy':
+        document.getElementById('healthy').appendChild(div);
+        break;
+      case 'international':
+        document.getElementById('international').appendChild(div);
+        break;
+      case 'meat lover':
+        document.getElementById('meat').appendChild(div);
+        break;
+      default:
+        document.getElementById('popular').appendChild(div);
+        break;
+    }
+  })
+}
 
-export { addRecipe, saveRecipe };
+
+export { addRecipe, saveRecipe, renderRecipe };
